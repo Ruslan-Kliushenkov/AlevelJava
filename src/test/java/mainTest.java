4f0fc37ccc97832775590d04c172e6d190d433ea
Ruslan-Kliushenkov <ruslan.klyshenkov@gmail.com>
2021-05-29 12:34:17 +0300

    Homework 9 1.0

--- src/test/java/mainTest.java
+++ src/test/java/mainTest.java
@@ -1,20 +1,23 @@
 
 import com.ua.ChessTurn.Convertor;
 import com.ua.ChessTurn.Chess;
-import com.ua.ChessTurn.Knight;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import static com.ua.ChessTurn.Chess.checkForCorrect;
+import static com.ua.ChessTurn.Convertor.getIndex;
+import static com.ua.ChessTurn.Knight.checkCondition;
 import static org.mockito.ArgumentMatchers.any;
 
-
+@RunWith(MockitoJUnitRunner.class)
 public class mainTest {
     @Mock
-    Convertor convertMock = Mockito.spy(Convertor.class);
-    Knight knight = new Knight();
+    Convertor convertMock;
     @InjectMocks
     Chess unit = new Chess(convertMock);
 
@@ -27,7 +30,7 @@ public class mainTest {
 
     @Test
     public void Condition(){
-        boolean result = knight.checkCondition(new int[] {0,1}, new int[] {1,0});
+        boolean result = checkCondition(new int[] {0,1}, new int[] {1,0});
         Assert.assertFalse(result);
     }
 
